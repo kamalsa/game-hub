c94dab32d817ed885d97a3c464f4ce817890c98c
Kamal SARRHINI <kamal.sarrhini@gmail.com>
2024-06-09 04:29:34 +0100

    Customizing the Chakra Theme

---
+++ src/data/genres.ts
@@ -0,0 +1,45 @@
+export default {
+    "id": 4,
+    "name": "Action",
+    "slug": "action",
+    "games_count": 180393,
+    "image_background": "https://media.rawg.io/media/games/587/587588c64afbff80e6f444eb2e46f9da.jpg",
+    "games": [
+      {
+        "id": 3498,
+        "slug": "grand-theft-auto-v",
+        "name": "Grand Theft Auto V",
+        "added": 20953
+      },
+      {
+        "id": 3328,
+        "slug": "the-witcher-3-wild-hunt",
+        "name": "The Witcher 3: Wild Hunt",
+        "added": 20396
+      },
+      {
+        "id": 5286,
+        "slug": "tomb-raider",
+        "name": "Tomb Raider (2013)",
+        "added": 16634
+      },
+      {
+        "id": 13536,
+        "slug": "portal",
+        "name": "Portal",
+        "added": 16415
+      },
+      {
+        "id": 12020,
+        "slug": "left-4-dead-2",
+        "name": "Left 4 Dead 2",
+        "added": 16295
+      },
+      {
+        "id": 5679,
+        "slug": "the-elder-scrolls-v-skyrim",
+        "name": "The Elder Scrolls V: Skyrim",
+        "added": 15780
+      }
+    ]
+  }
